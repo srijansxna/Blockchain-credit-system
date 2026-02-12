11435c227d6cda72a47a50ea7b225f39f1e456e7
Srijan Saxena <srijansaxena@srijans-MacBook-Air.local>
2026-02-12 22:16:31 +0530

    Deployment setup changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ import app from "./app.js";
 
 console.log("BANK_MASTER_KEY loaded:", !!process.env.BANK_MASTER_KEY);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 
 app.listen(PORT, () => {
   console.log(`Backend running on port ${PORT}`);
